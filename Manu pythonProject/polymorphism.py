8e3bcbc215ef7b4cfc22c30305660e487783770e
Emash1 <70946338+Emash1@users.noreply.github.com>
2024-03-25 09:25:35 -0400

    Add files via upload

---
+++ Manu pythonProject/polymorphism.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self):
+        pass
+
+    def speak(self):
+        print("I am an animal and I speak")
+
+class Dog(Animal):
+    def __init__(self):
+        pass
+    def speak(self):
+        print("I am a dog and I bark")
+
+def cat(Animal):
+    def __init__(self):
+        pass
+    def speak(self):
+        print("I am a cat and I meow")
+
+Dog1=Dog()
+Dog1.speak()
